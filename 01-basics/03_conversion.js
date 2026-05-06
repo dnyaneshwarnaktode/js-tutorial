b935997dd87fad7b0f820d858b1f950c5875a971
Dnyaneshwar <164074478+dnyaneshwarnaktode@users.noreply.github.com>
2026-05-06 20:25:33 +0000

    basic conversion

---
+++ 01-basics/03_conversion.js
@@ -0,0 +1,29 @@
+let score = true
+
+// console.log(typeof score);
+
+// let valueinNumber = Number(score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);  // Nan
+
+
+// number "33" ko convert krte hai to vo easily convert hojata hai 33 me
+// "33abc" => NaN
+// true => 2 false => 0
+
+// let isLogin = "pranay"
+// let booleanIslogin = Boolean(isLogin)
+// console.log(booleanIslogin);
+
+
+// 1 => true; 0=>false
+// ""  => false
+// "string" => true
+
+let somenumber =45
+console.log(typeof somenumber);
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
