1f1221f87468434dda7adf03dbb7e1e477c7f1f0
Dnyaneshwar <164074478+dnyaneshwarnaktode@users.noreply.github.com>
2026-05-13 18:24:28 +0000

    mahts

---
+++ 01-basics/06_nums_maths.js
@@ -0,0 +1,26 @@
+const score =400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log( balance.toString().length);
+// console.log( balance.toFixed(2));
+
+const otherNumber = 23.727356
+// console.log((otherNumber.toPrecision(3)));
+
+// maths
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.656));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(3,4,52,1,5));
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
