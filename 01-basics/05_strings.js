59b120524098fc7abc7e1a082007171fe1b695c6
Dnyaneshwar <164074478+dnyaneshwarnaktode@users.noreply.github.com>
2026-05-13 18:04:11 +0000

    strings part

---
+++ 01-basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "pranay"
+const repoCount = 50
+
+// console.log(name + repoCount + "value");
+console.log(`helllo my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('pranya-naktode-in')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('y'));
+
+console.log(gameName.split('-'));
+
