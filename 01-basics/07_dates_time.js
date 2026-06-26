f626f2210b47fe92451160c2714aa52d21f9d0ce
Dnyaneshwar <164074478+dnyaneshwarnaktode@users.noreply.github.com>
2026-06-26 05:16:01 +0000

    yes

---
+++ 01-basics/07_dates_time.js
@@ -0,0 +1,7 @@
+// dates 
+let myDates = new Date()
+console.log(myDates);
+console.log(myDates.toString());
+console.log(myDates.toDateString());
+console.log(myDates.toLocaleString());
+console.log(myDates.toLocaleDateString());
